7aad2a7453aecd297c84e3578d9c08076bb4d499
Gabriel vaz <joao.g.oliveira125@aluno.senai.br>
2026-09-16 20:09:06 -0300

    feat: implementa tratamento global

--- aula04-tratamento-global-exececoes/server.js
+++ aula04-tratamento-global-exececoes/server.js
@@ -14,7 +14,7 @@ app.get('/sucesso', (req , res) => {
     res.json({success: true, message: 'Operação Realizada Com Sucesso!'});
 });
 
-app.get('/erro-soncrono', (req, res, next) => {
+app.get('/erro-sincrono', (req, res, next) => {
     try{
         throw new Error('Falha ao processar a regra de negócio!')
     } catch(erro){
@@ -22,10 +22,22 @@ app.get('/erro-soncrono', (req, res, next) => {
     }
 });
 
-app.get('/erro-assincrono', async (req,res,next) => {
+app.get('/erro-assincrono', async (req, res, next) => {
     try{
         await Promisse.reject(new Error('Erro na cunsolta no banco de dados externo'));
     }catch(erro){
         next(erro);
     }
 });
+
+app.use((err,req,res,next) => {
+    console.error(`[LOG DE ERRO INTERNO]: ${err.stack}`);
+
+    const status = err.status || 500;
+    res.status(status).json({
+        success: false,
+        message: err.message || 'Erro interno no servidor'
+    });
+});
+
+app.listen(3000, ()=>{console.log('Servidor imortal rodando na porta 3000')});
