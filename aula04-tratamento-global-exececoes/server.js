cc7918a25fec67f12e4f8f5d38ca7aad64caa49b
Gabrielly Amaral <gabyamaral2805@gmail.com>
2026-09-16 18:52:51 -0300

    feat: implementa tratamento global

--- aula04-tratamento-global-exececoes/server.js
+++ aula04-tratamento-global-exececoes/server.js
@@ -9,3 +9,23 @@ process.on('uncaughtException', (err) => {
 process.on('unhandledRejection', (reason) => {
     console.error('[PROMISSE REJEITADA - unhandledRejection]:', reason);
 });
+
+app.get('/sucesso', (req , res) => {
+    res.json({success: true, message: 'Operação Realizada Com Sucesso!'});
+});
+
+app.get('/erro-soncrono', (req, res, next) => {
+    try{
+        throw new Error('Falha ao processar a regra de negócio!')
+    } catch(erro){
+        next(erro);
+    }
+});
+
+app.get('/erro-assincrono', async (req,res,next) => {
+    try{
+        await Promisse.reject(new Error('Erro na cunsolta no banco de dados externo'));
+    }catch(erro){
+        next(erro);
+    }
+});
